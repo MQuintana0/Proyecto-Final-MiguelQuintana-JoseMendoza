e8736217a004bd82e59a0eb01e0523e4f77bdeeb
MQuintana0 <quintanamiguel089@gmail.com>
2025-11-26 07:52:10 -0500

    Subida del proyecto

---
+++ src/main/java/co/edu/unicordoba/Proyecto_Final_MiguelQuintana_JoseMendoza/ProyectoFinalMiguelQuintanaJoseMendozaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicordoba.Proyecto_Final_MiguelQuintana_JoseMendoza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalMiguelQuintanaJoseMendozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalMiguelQuintanaJoseMendozaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicordoba/Proyecto_Final_MiguelQuintana_JoseMendoza/ProyectoFinalMiguelQuintanaJoseMendozaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicordoba.Proyecto_Final_MiguelQuintana_JoseMendoza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalMiguelQuintanaJoseMendozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
